19e7a94f972e56a82c16885770c1dc1a33b65bd8
Michael Frederick <master16789@gmail.com>
2016-11-14 17:14:02 -0500

    Add Jump Script to Player
    For example

---
+++ Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpHeightForce;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb2d.AddForce(Vector2.up * jumpHeightForce);
+		}
+	}
+}
