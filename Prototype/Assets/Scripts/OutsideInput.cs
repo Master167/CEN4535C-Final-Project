a147106f0a866d96c0666eb381aa82d7859bf28d
Michael Frederick <master16789@gmail.com>
2016-11-15 20:48:00 -0500

    Add Outside input.
    We have a microphone that will get the level that we want.

--- Prototype/Assets/Scripts/OutsideInput.cs
+++ Prototype/Assets/Scripts/OutsideInput.cs
@@ -2,15 +2,39 @@
 using System.Collections;
 
 public class OutsideInput : MonoBehaviour {
-	
 
-	// Use this for initialization
+    public string deviceName;
+    public float level;
+
+    private AudioSource source;
+    
 	void Start () {
-	
+        source = GetComponent<AudioSource>();
+        source.clip = Microphone.Start(null, true, 10, 44100);
+        source.loop = true;
+        source.mute = true;
+        while (!(Microphone.GetPosition(null) > 0))
+        {
+        }
+        source.Play();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        level = GetInputLevel() * 100;
 	}
+
+    public float GetInputLevel()
+    {
+        float[] data = new float[256];
+        float average = 0;
+        source.GetOutputData(data, 0);
+        foreach (float f in data)
+        {
+            average += Mathf.Abs(f);
+        }
+        return average / data.Length;
+        
+    }
 }
