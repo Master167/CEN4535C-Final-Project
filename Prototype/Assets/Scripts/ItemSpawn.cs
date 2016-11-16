9e6db3b78725b8a71cd74623f9defe59e46417f2
meganmolumby <n00942101@CISL-iM3-7.local>
2016-11-16 16:12:52 -0500

    Items going across screen

---
+++ Prototype/Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawn : MonoBehaviour {
+	public float delay = 0.1f;
+	public GameObject item;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", delay, delay);
+	
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+		Instantiate(item, new Vector3(-1, 0, 0), Quaternion.identity);
+	
+	}
+}
