4c95b07ab9940304d87c2a8260623bbc22b82177
Michael Frederick <master16789@gmail.com>
2016-11-21 17:33:08 -0500

    OutsideInput will set the level to the average of past inputs

--- Prototype/Assets/Scripts/OutsideInputSlider.cs
+++ Prototype/Assets/Scripts/OutsideInputSlider.cs
@@ -8,35 +8,55 @@ public class OutsideInputSlider : MonoBehaviour {
 	public float level = 0.5f;
 
 	private GameObject mainMenu;
-	private float[] inputs = new float[10];
+	private float[] inputs = {-1f, -1f, -1f, -1f, -1f, -1f, -1f, -1f, -1f, -1f};
+	private int index = 0;
 
 	void Start () {
 		mainMenu = GameObject.Find("GameManager");
 		MainMenu menu = mainMenu.GetComponent<MainMenu> ();
 		if (menu.isHard) {
-			level = 0.2f;
+			this.inputs[this.index] = 0.2f;
 		} else {
-			level = 0.5f;
+			this.inputs[this.index] = 0.5f;
 		}
-	}
-
-	void FixedUpdate () {
-		float input = Input.GetAxisRaw ("Vertical");
-		if (input > 0f) {
-			level = level + 0.01f;
-			if (level > 1f) {
-				level = 1;
-			}
-		} else if (input < 0) {
-			level = level - 0.01f;
-			if (level < 0) {
-				level = 0;
-			}
-		}
+		InvokeRepeating ("GetInput", 0f, 1.0f);
 	}
 
 	void Update() {
+		level = normalizeLevel ();
 		slider.normalizedValue = level;
+	}
+
+	void GetInput() {
+		float input = Input.GetAxisRaw ("Vertical");
+		float currentLevel = this.level;
+		if (input > 0f) {
+			currentLevel = currentLevel + 0.1f;
+			if (currentLevel > 1f) {
+				currentLevel = 1;
+			}
+		} else if (input < 0) {
+			currentLevel = currentLevel - 0.1f;
+			if (currentLevel < 0) {
+				currentLevel = 0;
+			}
+		}
+		this.index = (index + 1) % this.inputs.Length;
+		this.inputs [this.index] = currentLevel;
+
+	}
+
+	float normalizeLevel() {
+		int values = 1;
+		float total = this.inputs[this.index];
+		for (int i = 0; i < inputs.Length; i++) {
+			if (inputs [i] >= 0 && i != this.index) {
+				total += inputs [i];
+				values++;
+			}
+		}
+
+		return total / values;
 	}
 
 }
