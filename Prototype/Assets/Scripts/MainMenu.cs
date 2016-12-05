e3765a064da1cc842e72cd9a22970f56475943dd
meganmolumby <n00942101@CISL-iM3-7.local>
2016-12-05 16:04:36 -0500

    Fix C# scripts

---
+++ Prototype/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	private Button easyButton;
+	private Button hardButton;
+	private Button menuButton;
+	private GameObject player;
+	private Text timerText;
+	private float score;
+	private float currentTime;
+
+	public bool isHard = true;
+	public float maxTime = 120f;
+
+
+	// Use this for initialization
+	void Start () {
+		Object.DontDestroyOnLoad (this);
+	}
+
+	void Update()
+	{
+		if (SceneManager.GetActiveScene().name == "MainScene")
+		{
+			UpdateTimer();
+			// Gonna wait till player is on the ground before moving on
+			if (currentTime == 0 && !player.GetComponent<PlayerController>().isAirbourne)
+			{
+				MoveToScoreboard();
+			}
+		}
+	}
+
+	// Use this to attach scene events
+	void OnEnable() {
+		//SceneManager.sceneLoaded += setupScene;
+	}
+
+	// Use this to detach scene events
+	void OnDisable() {
+		//SceneManager.sceneLoaded -= setupScene;
+	}
+
+	void setupScene(Scene scene, LoadSceneMode mode) {
+		if (scene.name == "MainScene") {
+			player = GameObject.Find ("Player");
+			timerText = GameObject.Find("Timer").GetComponent<Text>();
+			currentTime = maxTime;
+		} else if (scene.name == "Scoreboard") {
+			menuButton = GameObject.Find ("MenuButton").GetComponent<Button> ();
+			menuButton.onClick.AddListener (MoveToMenu);
+			GameObject.Find ("Score").GetComponent<Text> ().text = "Final Score: " + score;
+		} else if (scene.name == "MainMenu") {
+			easyButton = GameObject.Find ("Easy").GetComponent<Button> ();
+			hardButton = GameObject.Find ("Hard").GetComponent<Button> ();
+			easyButton.onClick.AddListener (loadEasy);
+			hardButton.onClick.AddListener (loadHard);
+		}
+	}
+
+	void loadEasy() {
+		isHard = false;
+		MoveToMainScene ();
+	}
+
+	void loadHard() {
+		isHard = true;
+		MoveToMainScene ();
+	}
+
+	void MoveToMainScene() {
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	void MoveToMenu() {
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	void MoveToScoreboard()
+	{
+		score = player.GetComponent<PlayerController>().playerScore;
+		SceneManager.LoadScene("Scoreboard");
+	}
+
+	void UpdateTimer()
+	{
+		currentTime = currentTime - Time.deltaTime;
+		if (currentTime < 0)
+		{
+			currentTime = 0;
+		}
+		else if (currentTime < 30)
+		{
+			timerText.color = Color.red;
+		}
+		timerText.text = "Time: " + string.Format("{0}:{1:00}", (int)currentTime / 60, (int)currentTime % 60);
+	}
+
+}
---
+++ Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpHeightForce;
+	public GameObject outsideInput;
+	public float playerScore = 0;
+	public bool isAirbourne;
+
+	//Animation Variables
+	Animator anim;
+	public float jumpTime, jumpDelay = .5f;
+	public bool jumped;
+
+	private Rigidbody2D rb2d;
+	private bool isGrounded;
+	private float initialJumpHeightForce;
+
+	// Use this for initialization
+	void Start () {
+		this.rb2d = GetComponent<Rigidbody2D> ();
+		this.initialJumpHeightForce = jumpHeightForce;
+
+		//Animations
+		anim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		OutsideInputSlider script = outsideInput.GetComponent<OutsideInputSlider> ();
+		if (script != null) {
+			jumpHeightForce = initialJumpHeightForce * script.level;
+		} else {
+			jumpHeightForce = initialJumpHeightForce * .5f;
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+			rb2d.AddForce(Vector2.up * jumpHeightForce);
+			isGrounded = false;
+			isAirbourne = true;
+
+			//animation to change to jumping upon space bar
+			jumpTime = jumpDelay;
+			anim.SetTrigger ("jump");
+			jumped = true;
+		}
+
+
+		//animation jumptimer
+		jumpTime -= Time.deltaTime;
+		if (jumpTime <= 0 && isGrounded && jumped) {
+			anim.SetTrigger ("land");
+			jumped = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag ("Ground")) {
+			isGrounded = true;
+			isAirbourne = false;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.CompareTag ("Collectible")) {
+			Item item = collision.gameObject.GetComponent<Item> ();
+			if (item != null) {
+				playerScore += item.score;
+			}
+			Destroy (collision.gameObject, 0f);
+		}
+	}
+}
